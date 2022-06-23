72f050eca4999bcc2ff2e9395ad757364b0e8744
ISMashtakov <ismashtakov@edu.hse.ru>
2022-06-23 15:42:44 +0300

    added comment for ViewModel methods

--- SIGameMaker/ViewModels/Base/ViewModel.cs
+++ SIGameMaker/ViewModels/Base/ViewModel.cs
@@ -9,12 +9,14 @@ public class ViewModel: INotifyPropertyChanged, IDisposable
 {
     public event PropertyChangedEventHandler? PropertyChanged;
 
+    /// <summary>Notify that value are changed</summary>
     [NotifyPropertyChangedInvocator]
     protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
     {
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
-
+    
+    /// <summary>Set field value and notify subscribers</summary>
     protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
     {
         if(Equals(field, value)) return false;
