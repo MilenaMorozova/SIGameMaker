8febe53088bcea29095d27f65d3fe17b33768a3e
ISMashtakov <ismashtakov@edu.hse.ru>
2022-06-23 21:32:49 +0300

    Added property to RoundsHierarchy

--- SIGameMaker/Views/Components/RoundsHierarchy.xaml.cs
+++ SIGameMaker/Views/Components/RoundsHierarchy.xaml.cs
@@ -1,11 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Windows;
 using System.Windows.Controls;
+using SIGameMaker.Models;
 
 namespace SIGameMaker.Views.Components;
 
 public partial class RoundsHierarchy : UserControl
 {
-    public RoundsHierarchy()
+    public static readonly DependencyProperty RoundsProperty = DependencyProperty.Register(
+        nameof(Rounds),
+        typeof(IEnumerable<Round>),
+        typeof(RoundsHierarchy)
+    );
+
+    public IEnumerable<Round>? Rounds
     {
-        InitializeComponent();
+        get => (IEnumerable<Round>?)GetValue(RoundsProperty);
+        set => SetValue(RoundsProperty, value);
     }
+    public RoundsHierarchy() => InitializeComponent();
 }
