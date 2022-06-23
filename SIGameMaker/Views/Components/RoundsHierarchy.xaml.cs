dc06ffe4c6a49bc470c2f300b803c86e4f51da9f
ISMashtakov <ismashtakov@edu.hse.ru>
2022-06-23 20:26:40 +0300

    Code moved to another file

---
+++ SIGameMaker/Views/Components/RoundsHierarchy.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace SIGameMaker.Views.Components;
+
+public partial class RoundsHierarchy : UserControl
+{
+    public RoundsHierarchy()
+    {
+        InitializeComponent();
+    }
+}
